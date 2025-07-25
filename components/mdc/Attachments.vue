ec27748a057603781d2306e32bd149341c5c7ca7
Acfboy <AcfboyU@outlook.com>
2025-07-25 17:19:58 +0800

    feat: 附件列表可选参数

--- components/mdc/Attachments.vue
+++ components/mdc/Attachments.vue
@@ -1,19 +1,21 @@
 <template>
-    <v-expansion-panels v-if="attachs" :elevation="1" class="mt-2 mb-2" prepend-icon="mdi-paperclip">
-        <v-expansion-panel prepend-icon="mdi-paperclip">
-            <v-expansion-panel-title>
-                <!-- <v-icon>mdi-paperclip</v-icon> -->
-                附件
-            </v-expansion-panel-title>
-            <v-expansion-panel-text>
-                <v-list density="compact">
-                    <v-list-item v-for="a in attachs" :key="a.timestamp" density="compact"
-                        :href="`/api/files/${a.file_id}`" :title="a.name" :subtitle="a.timestamp"
-                        append-icon="mdi-paperclip" />
-                </v-list>
-            </v-expansion-panel-text>
-        </v-expansion-panel>
-    </v-expansion-panels>
+    <v-list :elevation="3" class="ma-2">
+        <v-list-group v-if="attachs">
+            <!-- eslint-disable-next-line vue/no-template-shadow -->
+            <template #activator="{ props }">
+                <v-list-item density="compact" v-bind="props" title="附件" prepend-icon="mdi-paperclip" />
+            </template>
+            <div v-if="!notSupport">
+                <v-list-item v-for="a in attachs" v-show="!props.fileIds || idList.find(f => f == a.file_id)"
+                    :key="a.timestamp" density="compact" :href="`/api/files/${a.file_id}`" :title="a.name"
+                    :subtitle="a.timestamp" append-icon="mdi-download" />
+            </div>
+            <v-list-item v-else>
+                附件列表等待审核。
+            </v-list-item>
+        </v-list-group>
+    </v-list>
+    <!-- </v-card> -->
 </template>
 
 <script setup lang="ts">
@@ -22,8 +24,13 @@ const majorId = route.params.major;
 const docId = route.params.doc;
 const requestFetch = useRequestFetch();
 
+const props = defineProps<{ fileIds: string | undefined }>();
+const notSupport = !docId || !majorId;
+
+const idList = computed<string[]>(() => JSON.parse(props.fileIds?.replaceAll(`'`, `"`) ?? ""));
+
 const { data: attachs } = useAsyncData(`attachments-${majorId}-${docId}-doc`, () =>
-    requestFetch<AttachmentInfo[]>("/api/files/with-doc", {
+    notSupport ? (Promise.resolve([])) : requestFetch<AttachmentInfo[]>("/api/files/with-doc", {
         method: "GET",
         query: {
             major_id: majorId,
